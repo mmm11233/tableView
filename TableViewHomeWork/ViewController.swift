4fb9874e5551f1d99ff23f15ab7c35008335a860
Mariam Joglidze <mariamjoglidze1@gmail.com>
2023-10-28 18:30:52 +0400

    Initial Commit

---
+++ TableViewHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewHomeWork
+//
+//  Created by Mariam Joglidze on 28.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
