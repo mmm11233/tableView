317a5b77c77908e0cafe0c1497bda02db58c7030
Mariam Joglidze <mariamjoglidze1@gmail.com>
2023-10-29 22:51:23 +0400

    first commit

---
+++ TableViewHomeWork/Scenes/AllBooksInfo/TableViewCell/MainViewControllerCell.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewControllerCell.swift
+//  TableViewHomeWork
+//
+//  Created by Mariam Joglidze on 28.10.23.
+//
+
+import UIKit
+
+class MainViewControllerCell: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
